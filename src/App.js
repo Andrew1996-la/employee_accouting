9557cc3853611ecfb3c30259cd8b1087972b0753
Andrew1996-la <asoborev@mail.ru>
2023-01-09 16:35:22 +0300

    создал структуру и верстку приложения

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import AppInfo from "./components/app-info/app-info";
+import AppFilter from "./components/app-filter/app-filter";
+import SearchPanel from "./components/search-panel/search-panel";
+import EmployeesList from "./components/employees-list/employees-list";
+import EmployeesAddForm from "./components/employees-add-form/employees-add-form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+          <AppInfo />
+
+          <div className='search-panel'>
+              <SearchPanel />
+              <AppFilter />
+          </div>
+
+          <EmployeesList />
+          <EmployeesAddForm />
+      </div>
   );
 }
 
-export default App;
+export default App
