bfdc2a1899df37c605411c73a25440ab65e09d05
Andrew1996-la <asoborev@mail.ru>
2023-01-13 17:45:01 +0300

    Реализовал удаление сотрудников из списка

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,21 +1,40 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 import employeesDB from "../../fakeDB/employeesDB";
+import { Component } from "react";
 
-const EmployeesList = () => {
-  return (
-    <ul className="app-list list-group">
-      {employeesDB.map((employee) => {
-        return (
-          <EmployeesListItem
-            key={employee.key}
-            name={employee.name}
-            salary={employee.salary}
-          />
-        );
-      })}
-    </ul>
-  );
-};
+class EmployeesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeesDB: employeesDB,
+    };
+  }
+
+  onDeleteEmployee = (key) => {
+    this.setState(({ employeesDB }) => ({
+      employeesDB: employeesDB.filter((employee) => employee.key !== key),
+    }));
+  };
+
+  render() {
+    const { employeesDB } = this.state;
+
+    return (
+      <ul className="app-list list-group">
+        {employeesDB.map((employee) => {
+          return (
+            <EmployeesListItem
+              key={employee.key}
+              name={employee.name}
+              salary={employee.salary}
+              onDeleteEmployee={() => this.onDeleteEmployee(employee.key)}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
+}
 
 export default EmployeesList;
